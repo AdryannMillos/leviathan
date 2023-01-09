de45198195ada5be099971a65d0285a208a34590
Adryann Millos Santos de Freitas <adryann.freitas@ubistart.com>
2023-01-09 10:26:08 -0300

    fea: Add footer

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,16 +2,29 @@
   <div>
     <Navbar></Navbar>
     <router-view />
+    <Footer></Footer>
   </div>
 </template>
 <script>
 import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   name: 'App',
   components: {
     Navbar,
+    Footer
   },
 };
 </script>
-<style></style>
+<style>
+html,
+body {
+  height: 100%;
+}
+
+#page-content {
+  flex: 1 0 auto;
+}
+
+</style>
