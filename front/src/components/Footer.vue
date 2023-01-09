f8951943f6953ae648edcc99eed591cfd46e9551
Adryann Millos Santos de Freitas <adryann.freitas@ubistart.com>
2023-01-09 10:26:26 -0300

    feat: Add footer

---
+++ front/src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer id="sticky-footer" class="flex-shrink-0 py-4 text-white-50">
+    <div class="container text-center text-white">
+      <small>Copyright &copy; Adryann Millos</small>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "FooterComponent",
+};
+</script>
+
+<style>
+.form-control {
+  width: 50%;
+}
+footer{
+    background-color: purple;
+    width: 100%;
+}
+</style>
