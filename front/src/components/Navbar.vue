35f2761dcc3e08206c3ac6a9b2c47fa1762252ef
Adryann Millos Santos de Freitas <adryann.freitas@ubistart.com>
2023-01-09 07:33:31 -0300

    fea: Add hrefs and colors to nav-items

--- front/src/components/Navbar.vue
+++ front/src/components/Navbar.vue
@@ -1,38 +1,50 @@
 <template>
   <nav class="navbar navbar-expand-lg">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Leviathan</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">GitHub</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
+    <div class="container-fluid">
+      <a class="navbar-brand text-white" href="/">Home</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a
+              class="nav-link text-white"
+              target="_blank"
+              href="https://leviathancommander.wixsite.com/home"
+              >Leviathan</a
+            >
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link text-white"
+              target="_blank"
+              href="https://github.com/AdryannMillos/leviathan/tree/main"
+              >GitHub</a
+            >
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 </template>
 
 <script>
 export default {
-    name: "NavbarComponent"
-}
+  name: "NavbarComponent",
+};
 </script>
 
 <style>
-.navbar{
-    background-color: purple;
+.navbar {
+  background-color: purple;
 }
 </style>
